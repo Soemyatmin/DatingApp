128fb7752b99b4b3b92e4234faefa67cb4f66b24
Soemyatmin <soemyatmin.nathan@gmail.com>
2022-05-08 16:34:51 +0630

    end of section 4

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,19 +3,21 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers {
     public class AccountController : BaseApiController {
-
         private readonly DataContext _context;
-        public AccountController(DataContext context) {
+        private readonly ITokenService _tokenService;
+        public AccountController(DataContext context, ITokenService tokenService) {
             _context = context;
+            _tokenService = tokenService;
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto) {
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto) {
             if (await UserExists(registerDto.Username)) {
                 return BadRequest("Username is taken");
             }
@@ -28,11 +30,14 @@ namespace API.Controllers {
             };
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
-            return user;
+            return new UserDto{
+                UserName = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto) {
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto) {
 
             var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
             if (user == null) {
@@ -47,7 +52,10 @@ namespace API.Controllers {
                     return Unauthorized("Invalid password");
                 }
             }
-            return user;
+            return new UserDto{
+                UserName = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };;
         }
 
         private async Task<bool> UserExists(string username) {
---
+++ API/Extionsions/IndentityServiceExtensions.cs
@@ -0,0 +1,21 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Extionsions {
+    public static class IndentityServiceExtensions {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config) {
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(option => {
+                option.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            return services;
+        }
+    }
+}
